6d02c4074748534fc471787b89e4002550842a6a
Kailuo Wang <kailuo.wang@gmail.com>
2012-04-12 16:21:18 -0300

    Update retrieve.js

--- retrieve.js
+++ retrieve.js
@@ -1,24 +1,26 @@
-function retrieveUrl(){
-  var version = 'z'; //change to 'b' for large version
-  var picUrls = _($('.rapidnofollow .pc_img')).map( 
-    function(pic){ return pic.src.replace('_s.' , '_' + version + '.') 
+var retrieveUrl;
+retrieveUrl = function(opts) {
+  var index, output, picUrls, versionCode;
+  if (opts == null) {
+    opts = {};
+  }
+  _.defaults(object, {
+    version: "medium",
+    lineBetween: 4
   });
-  var index = 1;
-  var linesBetween = 4;
-  var output = [];
-  _(picUrls).each( function(url) { 
+  versionCode = opts.version === "medium" ? "z" : "b";
+  picUrls = _($(".rapidnofollow .pc_img")).map(function(pic) {
+    return pic.src.replace("_s.", "_" + versionCode + ".");
+  });
+  index = 1;
+  output = [];
+  _(picUrls).each(function(url) {
     output.push(index);
     output.push(url);
-    printBlankLines(linesBetween, output);
-    index++;
+    _.times(opts.linesBetween, function() {
+      return output.push(' ');
+    });
+    return index++;
   });
-  return output.join("\n")
-  
-}
-
-function printBlankLines(lines, output){
-  _(_.range(lines)).each(function(line){
-    var spaces = _.range(line).map(function(){ return " "; }).join('');
-    output.push(spaces);
-  });
-}
+  return output.join("\n");
+};
